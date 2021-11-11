13950460f27088cf541218c303d01a5aadd3554d
atat072 <paulhenke46@gmail.com>
2021-11-11 12:15:22 +0100

    Set up the Main class and created the necessary Screens

---
+++ core/src/de/atat072/rpg/screens/CreditScreen.java
@@ -0,0 +1,7 @@
+package de.atat072.rpg.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class CreditScreen extends ScreenAdapter {
+
+}
---
+++ core/src/de/atat072/rpg/screens/GameScreen.java
@@ -0,0 +1,6 @@
+package de.atat072.rpg.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+}
