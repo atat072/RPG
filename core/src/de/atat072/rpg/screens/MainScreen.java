450cd5491e7032e26ab0551ce1cd358df503344d
atat072 <paulhenke46@gmail.com>
2021-11-11 14:23:19 +0100

    Added skins for the GUI

--- core/src/de/atat072/rpg/screens/MainScreen.java
+++ core/src/de/atat072/rpg/screens/MainScreen.java
@@ -1,7 +1,9 @@
 package de.atat072.rpg.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class MainScreen extends ScreenAdapter {
-
+    Skin skin = new Skin(Gdx.files.internal("gdx-skins-master/"));
 }
