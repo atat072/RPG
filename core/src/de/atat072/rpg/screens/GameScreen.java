bccc1d9b8f67f54bb2fb7509a4f36be5c94fc107
atat072 <paulhenke46@gmail.com>
2021-11-13 10:38:13 +0100

    GameScreen Layout is finished and the functions to add Text are implemented

--- core/src/de/atat072/rpg/screens/GameScreen.java
+++ core/src/de/atat072/rpg/screens/GameScreen.java
@@ -1,6 +1,90 @@
 package de.atat072.rpg.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import java.util.ArrayList;
 
 public class GameScreen extends ScreenAdapter {
+
+    private Skin skin = new Skin(Gdx.files.internal("gdx-skins-master/commodore64/skin/uiskin.json"));
+    private SpriteBatch batch;
+    private Stage stage;
+    private Table table,tableText, tableOptions;
+    private ScrollPane scrollText;
+    private ArrayList<Label> text;
+    private ArrayList<Label> options;
+
+    public GameScreen(){
+        initialise();
+        setLayout();
+    }
+
+    private void initialise(){
+        batch = new SpriteBatch();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        table = new Table(skin);
+        table.background("window");
+        table.setFillParent(true);
+        //table.debug();
+        tableText = new Table(skin);
+        //tableText.debug();
+        tableOptions = new Table(skin);
+        tableOptions.background("dialog");
+        //tableOptions.debug();
+        scrollText = new ScrollPane(null,skin);
+        //scrollText.debug();
+        scrollText.setScrollbarsVisible(true);
+        text = new ArrayList<>();
+        options = new ArrayList<>();
+    }
+
+    private void setLayout(){
+        stage.addActor(table);
+        table.add(scrollText).expand().fill().pad(10);
+        scrollText.setActor(tableText);
+        for(Label l:text){
+            tableText.add(l).expandX().fillX().pad(10);
+            tableText.row();
+        }
+        table.row();
+        table.add(tableOptions).expand().fill().pad(10);
+        for(Label l:options){
+            tableText.add(l).expandX().fillX().pad(10);
+            tableText.row();
+        }
+    }
+
+    @Override
+    public void render(float delta){
+        stage.act(delta);
+        setLayout();
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void dispose(){
+        skin.dispose();
+        stage.dispose();
+        batch.dispose();
+    }
+
+    public void setText(String newText){
+        text.add(new Label(newText, skin));
+    }
+
+    public void setOption(ArrayList<String> newOptions){
+        options.clear();
+        for(String s: newOptions){
+            options.add(new Label(s,skin));
+        }
+    }
 }
